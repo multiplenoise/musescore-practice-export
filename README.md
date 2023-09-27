8d8a82ae725adae810b0299546866e5380c33d76
Robin Elfrink <robin@15augustus.nl>
2023-09-27 19:47:24 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# PracticeExport
+
+This plugin will export your score as PDF, Midi, MusicXML and MP3. Individual
+staff export to MP3 is done as well, while lowering the volume of other staffs
+for easy rehearsal.
+
+Additionally the plugin can add a metronome to the MP3 exports.
+
+## Compatibility
+
+The plugin currently only works with MuseScore 3.6 due to
+[missing API functionality](https://github.com/musescore/MuseScore/issues/13593)
+in MuseScore 4.
---
+++ translations/locale_nl.ts
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+<context>
+    <name>PracticeExport</name>
+    <message>
+        <location filename="../PracticeExport.qml" line="11"/>
+        <location filename="../PracticeExport.qml" line="27"/>
+        <source>Export practice files</source>
+        <translation type="finished">Exporteer oefenbestanden</translation>
+    </message>
+    <message>
+        <location filename="../PracticeExport.qml" line="16"/>
+        <location filename="../PracticeExport.qml" line="20"/>
+        <source>Export score as PDF, Midi, MusicXML and individual staff MP3s.</source>
+        <translation type="finished">Exporteer partituur naar PDF, Midi, MusicXML en individuele MP3&apos;s.</translation>
+    </message>
+    <message>
+        <location filename="../PracticeExport.qml" line="148"/>
+        <source>Volume for other instruments:</source>
+        <translation type="finished">Volume voor andere instrumenten</translation>
+    </message>
+    <message>
+        <location filename="../PracticeExport.qml" line="177"/>
+        <source>Base file name:</source>
+        <translation type="finished">Basis bestandsnaam:</translation>
+    </message>
+    <message>
+        <location filename="../PracticeExport.qml" line="196"/>
+        <source>Export to folder:</source>
+        <translation type="finished">Exporteer naar map:</translation>
+    </message>
+    <message>
+        <location filename="../PracticeExport.qml" line="211"/>
+        <source>Browse</source>
+        <translation type="finished">Blader</translation>
+    </message>
+    <message>
+        <location filename="../PracticeExport.qml" line="252"/>
+        <source>Select target folder</source>
+        <translation type="finished">Selecteer doelmap</translation>
+    </message>
+    <message>
+        <location filename="../PracticeExport.qml" line="269"/>
+        <source>Unsupported MuseScore Version</source>
+        <translation type="finished">Niet-ondersteunde MuseScore-versie</translation>
+    </message>
+    <message>
+        <location filename="../PracticeExport.qml" line="270"/>
+        <source>This plugin requires MuseScore 3.6.</source>
+        <translation type="finished">Deze plugin vereist MuseScore 3.6.</translation>
+    </message>
+    <message>
+        <location filename="../PracticeExport.qml" line="277"/>
+        <source>Finished</source>
+        <translation type="finished">Klaar</translation>
+    </message>
+    <message>
+        <location filename="../PracticeExport.qml" line="278"/>
+        <source>Export of practice files has finished.</source>
+        <translation type="finished">Exporteren van oefenbestanden is klaar.</translation>
+    </message>
+</context>
+</TS>
